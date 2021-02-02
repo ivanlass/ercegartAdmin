4af4437fb2b31bb050ca2d8602483105af66858c
Your Name <you@example.com>
2021-02-02 23:19:14 +0100

    logo changed

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -4,29 +4,29 @@ import '../App.css';
 
 
 function Aboutus() {
-  return (
-      <>
-    <div className="aboutus">
-        <div>
-            <h1>About us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+    return (
+        <>
+            <div className="aboutus">
+                <div>
+                    <h1>About us</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
             </p>
-        </div>
-        <div>
-            <h1>About us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                </div>
+                <div>
+                    <h1>About us</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
             </p>
-        </div>
-    </div>
-            <svg  viewBox="0 0 1440 405" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1439 42.5H-1V276.243C199.996 -39.9597 340.809 439.297 397.638 276.243C454.468 113.189 684.906 77.973 880.806 151.28C1076.71 224.588 1110.37 212.807 1258.44 261.653C1406.5 310.5 1435.83 424.374 1439 401V42.5Z" fill="#666666" fill-opacity="0.1"/>
-            <path d="M0 3H1440V204.044C1239 -67.9242 1098.19 344.288 1041.36 204.044C984.532 63.8001 754.094 33.5106 558.194 96.5628C362.294 159.615 257.962 131.474 180.563 204.044C103.164 276.614 84.6981 132.013 0 225V3Z" fill="#242424" fill-opacity="0.3"/>
-            <path d="M1440 0H0V43.0141C280.984 36.1184 509.935 63.2364 586.5 32.1626C663.065 1.08882 756.825 35.4404 881.806 20.0181C1006.79 4.59582 1172.05 28.9881 1259.44 61.5432C1371.02 103.113 1436.83 91.4876 1440 87.1862V0Z" fill="#242424"/>
+                </div>
+            </div>
+            <svg className="brdo" viewBox="0 0 1440 405" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1439 42.5H-1V276.243C199.996 -39.9597 340.809 439.297 397.638 276.243C454.468 113.189 684.906 77.973 880.806 151.28C1076.71 224.588 1110.37 212.807 1258.44 261.653C1406.5 310.5 1435.83 424.374 1439 401V42.5Z" fill="#666666" fill-opacity="0.1" />
+                <path d="M0 3H1440V204.044C1239 -67.9242 1098.19 344.288 1041.36 204.044C984.532 63.8001 754.094 33.5106 558.194 96.5628C362.294 159.615 257.962 131.474 180.563 204.044C103.164 276.614 84.6981 132.013 0 225V3Z" fill="#242424" fill-opacity="0.3" />
+                <path d="M1440 0H0V43.0141C280.984 36.1184 509.935 63.2364 586.5 32.1626C663.065 1.08882 756.825 35.4404 881.806 20.0181C1006.79 4.59582 1172.05 28.9881 1259.44 61.5432C1371.02 103.113 1436.83 91.4876 1440 87.1862V0Z" fill="#242424" />
             </svg>
-            </>
-  );
+        </>
+    );
 }
 
 export default Aboutus;
