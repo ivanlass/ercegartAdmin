b09151cb9e7584ea2634a8d02d14b8943223bed5
ivanlass <ivansky56@gmail.com>
2022-12-02 15:55:06 +0100

    changed api url

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,64 +1,56 @@
-import React, {useState, useEffect} from 'react'
-import '../Card.css'
-import Carousels from '../components/Carousel';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import {useParams} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "../Card.css";
+import Carousels from "../components/Carousel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useParams } from "react-router-dom";
 
 function Product() {
-    const [product, setProduct] = useState(null)
-    let { id } = useParams();
+  const [product, setProduct] = useState(null);
+  let { id } = useParams();
 
-    useEffect(()=>{
-        fetch('https://ercegart.herokuapp.com/products/findone', {
-          method: 'POST', // or 'PUT'
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({query:id}),
-        })
-        .then(response => response.json())
-        .then(data => {
-          setProduct(data)
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      },[])
-
-
-
+  useEffect(() => {
+    fetch("https://ercegbackend-c42jv.ondigitalocean.app/products/findone", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
   return (
-      <>
-     {product &&
-    <div className="product">
-        <div>
-        <Carousels slike={product.slike}/>
-        </div>
-        <br/>
-        <Container>
-
+    <>
+      {product && (
+        <div className="product">
+          <div>
+            <Carousels slike={product.slike} />
+          </div>
+          <br />
+          <Container>
             <Row>
-                <Col lg="4" className="product-info-sm">
-                    <p>{product.name.hr}</p>
-                </Col>
-                <Col className="product-info-big">
-                    <p className="materials-title">Materijali</p>
-                    <p className="materials-item">{product.materijali.hr}</p>
-                    <p className="materials-title">Opis</p>
-                    <p className="product-about">{product.opis.hr}</p>
-                </Col>
-
-
+              <Col lg="4" className="product-info-sm">
+                <p>{product.name.hr}</p>
+              </Col>
+              <Col className="product-info-big">
+                <p className="materials-title">Materijali</p>
+                <p className="materials-item">{product.materijali.hr}</p>
+                <p className="materials-title">Opis</p>
+                <p className="product-about">{product.opis.hr}</p>
+              </Col>
             </Row>
-        </Container>
-   
-        
-    </div>
-    }
-  </>
+          </Container>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/UljeNaPlatnu.js
+++ src/pages/UljeNaPlatnu.js
@@ -1,138 +1,199 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../components/Card'
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
-import { toast } from 'react-toastify';
-import '../App.css'
+import { toast } from "react-toastify";
+import "../App.css";
 
 function UljeNaPlatnu() {
-  const [products, setProducts] = useState(null)
-  const [isOpen, setIsopen] = useState(false)
-  const [name, setName] = useState("")
-  const [materijali, setMaterijali] = useState("")
-  const [slike, setSlike] = useState("")
-  const [opis, setOpis] = useState("")
-  const [nameEn, setNameEn] = useState("")
-  const [materijaliEn, setMaterijaliEn] = useState("")
-  const [opisEn, setOpisEn] = useState("")
-  const [isDisabled, setIsDisabled] = useState(false)
+  const [products, setProducts] = useState(null);
+  const [isOpen, setIsopen] = useState(false);
+  const [name, setName] = useState("");
+  const [materijali, setMaterijali] = useState("");
+  const [slike, setSlike] = useState("");
+  const [opis, setOpis] = useState("");
+  const [nameEn, setNameEn] = useState("");
+  const [materijaliEn, setMaterijaliEn] = useState("");
+  const [opisEn, setOpisEn] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
 
-
-  const toggleForm = () => setIsopen(!isOpen)
-
+  const toggleForm = () => setIsopen(!isOpen);
 
   const deleteProduct = (e) => {
-    fetch('https://ercegart.herokuapp.com/products/delete', {
-      method: 'POST', // or 'PUT'
+    fetch("https://ercegbackend-c42jv.ondigitalocean.app/products/delete", {
+      method: "POST", // or 'PUT'
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({query:e.currentTarget.dataset.id}),
+      body: JSON.stringify({ query: e.currentTarget.dataset.id }),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      setProducts(data)
-      toast("Obrisano");
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+        toast("Obrisano");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-
-
-  useEffect(()=>{
-    fetch('https://ercegart.herokuapp.com/products', {
-      method: 'POST', // or 'PUT'
+  useEffect(() => {
+    fetch("https://ercegbackend-c42jv.ondigitalocean.app/products", {
+      method: "POST", // or 'PUT'
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({query:"uljeNaPlatnu"}),
+      body: JSON.stringify({ query: "uljeNaPlatnu" }),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      setProducts(data)
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  },[])
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-
-  const sendData = e => {
-    e.preventDefault()
-    setIsDisabled(!isDisabled)
-    const formData = new FormData()
+  const sendData = (e) => {
+    e.preventDefault();
+    setIsDisabled(!isDisabled);
+    const formData = new FormData();
     for (let i = 0; i < slike.length; i++) {
-      formData.append("image", slike[i])
+      formData.append("image", slike[i]);
     }
 
-    console.log(slike)
-    formData.append('name', name)
-    formData.append("materijali", materijali)
-    formData.append("opis", opis)
-    formData.append("kategorija", "uljeNaPlatnu")
-    formData.append("nameEn", nameEn)
-    formData.append("materijaliEn", materijaliEn)
-    formData.append("opisEn", opisEn)
+    console.log(slike);
+    formData.append("name", name);
+    formData.append("materijali", materijali);
+    formData.append("opis", opis);
+    formData.append("kategorija", "uljeNaPlatnu");
+    formData.append("nameEn", nameEn);
+    formData.append("materijaliEn", materijaliEn);
+    formData.append("opisEn", opisEn);
 
     const config = {
       headers: {
-        'content-type': 'multipart/form-data'
-      }
+        "content-type": "multipart/form-data",
+      },
     };
-    fetch('https://ercegart.herokuapp.com/products/add', {
-      method: 'POST',
-      body: formData
+    fetch("https://ercegbackend-c42jv.ondigitalocean.app/products/add", {
+      method: "POST",
+      body: formData,
     })
-      .then(response => response.json())
-      .then(result => {
-        setIsDisabled(false)
-        setName("")
-        setNameEn("")
-        setOpis("")
-        setOpisEn("")
-        setMaterijali("")
-        setMaterijaliEn("")
+      .then((response) => response.json())
+      .then((result) => {
+        setIsDisabled(false);
+        setName("");
+        setNameEn("");
+        setOpis("");
+        setOpisEn("");
+        setMaterijali("");
+        setMaterijaliEn("");
         toast("Dodano ne sekiraj se nista.");
-        console.log('Success:', result);
+        console.log("Success:", result);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
-  }
-
-
+  };
 
   return (
     <>
-      <button className="open-btn" onClick={toggleForm}>Dodaj</button>
-      {isOpen &&
-        <form className="form" onSubmit={sendData} encType="multipart/form-data">
-          <button className="close-btn" onClick={toggleForm}>&#10005;</button>
+      <button className="open-btn" onClick={toggleForm}>
+        Dodaj
+      </button>
+      {isOpen && (
+        <form
+          className="form"
+          onSubmit={sendData}
+          encType="multipart/form-data"
+        >
+          <button className="close-btn" onClick={toggleForm}>
+            &#10005;
+          </button>
           <div className="all-inputs">
-            <input required value={name} className="input" placeholder="Ime proizvoda" onChange={e => setName(e.target.value)} />
-            <input required value={nameEn} className="input" placeholder="Ime na engleski" onChange={e => setNameEn(e.target.value)} />
-            <input required value={materijali} className="input" placeholder="Materijali" onChange={e => setMaterijali(e.target.value)} />
-            <input required value={materijaliEn} className="input" placeholder="Materijali ENG" onChange={e => setMaterijaliEn(e.target.value)} />
-            <input required  className="input" multiple type="file" onChange={(e) => setSlike(e.target.files)} />
-            <textarea required value={opis} className="input" placeholder="Opis" onChange={e => setOpis(e.target.value)} />
-            <textarea required value={opisEn} className="input" placeholder="Opis na ENG" onChange={e => setOpisEn(e.target.value)} />
-            <button className="form-button" type="submit" disabled={isDisabled}>{isDisabled ?<Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />: "SPREMI"}</button>
+            <input
+              required
+              value={name}
+              className="input"
+              placeholder="Ime proizvoda"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              required
+              value={nameEn}
+              className="input"
+              placeholder="Ime na engleski"
+              onChange={(e) => setNameEn(e.target.value)}
+            />
+            <input
+              required
+              value={materijali}
+              className="input"
+              placeholder="Materijali"
+              onChange={(e) => setMaterijali(e.target.value)}
+            />
+            <input
+              required
+              value={materijaliEn}
+              className="input"
+              placeholder="Materijali ENG"
+              onChange={(e) => setMaterijaliEn(e.target.value)}
+            />
+            <input
+              required
+              className="input"
+              multiple
+              type="file"
+              onChange={(e) => setSlike(e.target.files)}
+            />
+            <textarea
+              required
+              value={opis}
+              className="input"
+              placeholder="Opis"
+              onChange={(e) => setOpis(e.target.value)}
+            />
+            <textarea
+              required
+              value={opisEn}
+              className="input"
+              placeholder="Opis na ENG"
+              onChange={(e) => setOpisEn(e.target.value)}
+            />
+            <button className="form-button" type="submit" disabled={isDisabled}>
+              {isDisabled ? (
+                <Loader
+                  type="BallTriangle"
+                  color="#00BFFF"
+                  height={80}
+                  width={80}
+                />
+              ) : (
+                "SPREMI"
+              )}
+            </button>
           </div>
         </form>
-      }
+      )}
       <div className="gallery">
-        {products&& products.map(product=>{
-          return (
-            <div className="card-wrapper">
-              <button className="delete-btn" onClick={deleteProduct} data-id={product._id}>X</button>
-              <Card product={product} key={product._id}/>
-            </div>
-          )
-        })}
+        {products &&
+          products.map((product) => {
+            return (
+              <div className="card-wrapper">
+                <button
+                  className="delete-btn"
+                  onClick={deleteProduct}
+                  data-id={product._id}
+                >
+                  X
+                </button>
+                <Card product={product} key={product._id} />
+              </div>
+            );
+          })}
       </div>
     </>
   );
